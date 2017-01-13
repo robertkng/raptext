6a29d9a924462711761ac71b1b5f7ee07a4c143a
Robert Ng <robertng85@gmail.com>
2017-01-13 12:45:21 -0500

    Add components

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react", "jsx-a11y", "import"
+  ],
+
+  "env": {
+    "browser": true
+  },
+
+  "rules": {
+    "new-cap": [
+      2,
+      {
+        "capIsNewExceptions": [
+          "Map",
+          "List"
+        ]
+      }
+    ],
+    "import/no-extraneous-dependencies": 0,
+    "global-require": 0,
+    "react/forbid-prop-types": 0,
+    // I am adding this rule because eslint cannot resolve extension .scss
+    "import/extensions": 0,
+  },
+};
---
+++ src/Components/App/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: '',
+      // phoneNumber: '',
+    };
+    this.reset = this.reset.bind(this);
+  }
+
+  updateInput(e) {
+    this.setState = ({
+      searchTerm = e.target.value,
+    });
+  }
+
+  reset() {
+    this.setState({
+      searchTerm = '',
+    });
+  }
+
+  render() {
+    return (
+      <div id ="search">
+      <h1>Rap Text</h1>
+        <Search
+          searchTerm={this.state.searchTerm}
+          updateInput={this.updateInput.bind(this)}
+        />
+      </div>
+    );
+  }
+}
